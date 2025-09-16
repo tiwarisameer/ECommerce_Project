c4a0d3140503d7954e85f2c51a5ca19c5a49ba63
Sameer_Repo <sameer971999@gmail.com>
2025-09-16 22:57:03 +0530

    Project_Setup

---
+++ src/main/java/com/sameer/first_code/FirstCodeApplication.java
@@ -0,0 +1,13 @@
+package com.sameer.first_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sameer/first_code/FirstCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sameer.first_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
